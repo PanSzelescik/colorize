2458f9fca3bcd4e72157acd7e2f4d5328ce6a92b
PanSzelescik <panszelescik@gmail.com>
2022-09-04 16:52:18 +0200

    Show sneaking and consuming item in JEI, closes #8

--- common/src/main/java/pl/panszelescik/colorize/common/recipes/ColorizeRecipe.java
+++ common/src/main/java/pl/panszelescik/colorize/common/recipes/ColorizeRecipe.java
@@ -3,5 +3,5 @@ package pl.panszelescik.colorize.common.recipes;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 
-public record ColorizeRecipe(Ingredient validBlocks, Ingredient item, ItemStack result) {
+public record ColorizeRecipe(Ingredient validBlocks, Ingredient item, ItemStack result, boolean sneaking, boolean consume) {
 }
--- common/src/main/java/pl/panszelescik/colorize/common/recipes/jei/ColorizeJEICategory.java
+++ common/src/main/java/pl/panszelescik/colorize/common/recipes/jei/ColorizeJEICategory.java
@@ -5,13 +5,15 @@ import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.drawable.IDrawableStatic;
+import mezz.jei.api.gui.ingredient.IRecipeSlotTooltipCallback;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -21,13 +23,16 @@ public class ColorizeJEICategory implements IRecipeCategory<ColorizeRecipe> {
 
     public static final RecipeType<ColorizeRecipe> RECIPE_TYPE = RecipeType.create("colorize", "colorize", ColorizeRecipe.class);
 
-    public static final String TEXTURE_GUI_PATH = "textures/gui/";
-    public static final String TEXTURE_GUI_VANILLA = TEXTURE_GUI_PATH + "gui_vanilla.png";
-    public static final ResourceLocation RECIPE_GUI_VANILLA = new ResourceLocation(ModIds.JEI_ID, TEXTURE_GUI_VANILLA);
+    private static final Component TITLE = new TranslatableComponent("gui.colorize.jei.name");
+    private static final Component SNEAKING = new TranslatableComponent("gui.colorize.jei.sneaking").withStyle(ChatFormatting.ITALIC).withStyle(ChatFormatting.GRAY);
+    private static final Component CONSUME = new TranslatableComponent("gui.colorize.jei.consume").withStyle(ChatFormatting.ITALIC).withStyle(ChatFormatting.GRAY);
+
+    private static final String TEXTURE_GUI_PATH = "textures/gui/";
+    private static final String TEXTURE_GUI_VANILLA = TEXTURE_GUI_PATH + "gui_vanilla.png";
+    private static final ResourceLocation RECIPE_GUI_VANILLA = new ResourceLocation(ModIds.JEI_ID, TEXTURE_GUI_VANILLA);
 
     private final IDrawableStatic background;
     private final IDrawable icon;
-    private final Component title = new TextComponent("Colorize");
 
     public ColorizeJEICategory(IGuiHelper guiHelper) {
         // Use Anvil texture
@@ -42,7 +47,7 @@ public class ColorizeJEICategory implements IRecipeCategory<ColorizeRecipe> {
 
     @Override
     public Component getTitle() {
-        return this.title;
+        return TITLE;
     }
 
     @Override
@@ -57,17 +62,32 @@ public class ColorizeJEICategory implements IRecipeCategory<ColorizeRecipe> {
 
     @Override
     public void setRecipe(IRecipeLayoutBuilder builder, ColorizeRecipe recipe, IFocusGroup focuses) {
+        IRecipeSlotTooltipCallback callback = (recipeSlotView, tooltip) -> {
+            if (recipe.sneaking()) {
+                tooltip.add(SNEAKING);
+            }
+        };
+        IRecipeSlotTooltipCallback callback2 = (recipeSlotView, tooltip) -> {
+            callback.onTooltip(recipeSlotView, tooltip);
+            if (recipe.consume()) {
+                tooltip.add(CONSUME);
+            }
+        };
+
         builder.addSlot(RecipeIngredientRole.CATALYST, 1, 1)
                 .setSlotName("input")
-                .addIngredients(recipe.validBlocks());
+                .addIngredients(recipe.validBlocks())
+                .addTooltipCallback(callback);
 
         builder.addSlot(RecipeIngredientRole.INPUT, 50, 1)
                 .setSlotName("item")
-                .addIngredients(recipe.item());
+                .addIngredients(recipe.item())
+                .addTooltipCallback(callback2);
 
         builder.addSlot(RecipeIngredientRole.OUTPUT, 108, 1)
                 .setSlotName("output")
-                .addItemStack(recipe.result());
+                .addItemStack(recipe.result())
+                .addTooltipCallback(callback);
     }
 
     @Override
